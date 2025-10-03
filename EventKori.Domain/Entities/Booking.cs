4191ba184a973d1ac8431cec6bfaa25c611b4368
siddik-dev <siddik.iitju@gmail.com>
2025-10-04 01:33:56 +0600

    Removed unnecessary namespaces

--- EventKori.Domain/Entities/Booking.cs
+++ EventKori.Domain/Entities/Booking.cs
@@ -1,9 +1,4 @@
 ﻿using EventKori.Domain.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EventKori.Domain.Entities
 {
--- EventKori.Domain/Interfaces/IEventRepository.cs
+++ EventKori.Domain/Interfaces/IEventRepository.cs
@@ -1,10 +1,5 @@
 ﻿using EventKori.Domain.Entities;
 using EventKori.Domain.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EventKori.Domain.Interfaces
 {
